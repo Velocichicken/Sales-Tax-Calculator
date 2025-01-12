bd1db752df0138c49d5240bcc353c7722d068e1f
Velocichicken <velocichicken1@gmail.com>
2025-01-11 21:50:19 -0600

    service worker inprovements

--- service-worker.js
+++ service-worker.js
@@ -6,3 +6,21 @@ importScripts(
     ({request}) => request.destination === 'image',
     new workbox.strategies.CacheFirst()
   );
+
+  workbox.routing.registerRoute(
+    '/manifest.json',
+    new workbox.strategies.NetworkFirst()
+  );
+
+  workbox.routing.registerRoute(
+    ({url}) => url.pathname.startsWith('/icons/'),
+    new workbox.strategies.CacheFirst({
+      cacheName: 'splash-images',
+      plugins: [
+        new workbox.expiration.ExpirationPlugin({
+          maxEntries: 50, // Adjust based on your app's needs
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        }),
+      ],
+    })
+  );
