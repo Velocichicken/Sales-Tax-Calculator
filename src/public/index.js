000e83ab3bc0f563cf3e86270ba73f2fe9e98508
Velocichicken <velocichicken1@gmail.com>
2025-01-11 20:27:55 -0600

    first commit

---
+++ src/public/index.js
@@ -0,0 +1,64 @@
+const itemNameInput = document.getElementById('ItemNameInput');
+const itemPriceInput = document.getElementById('ItemPriceInput');
+
+const enterItemButton = document.getElementById('lebutton');
+const removeItemButton = document.getElementById('RemoveButton');
+
+const itemDisplay = document.getElementById('ItemDisplay');
+const totalDisplay = document.getElementById('TotalDisplay');
+
+itemNameInput.setAttribute("placeholder", "Enter Item Name");
+itemPriceInput.setAttribute("placeholder", "Enter Item Price");
+
+
+let itemNames = [];
+let itemPrices = [];
+
+let tax = 0.10;
+
+
+const express = require('express')
+const app = express()
+
+app.use(express.static("./src"))
+
+app.listen(7080, ()=>{
+    console.log("ready")
+})
+
+
+enterItemButton.onclick = function()
+{
+    itemNames.push(itemNameInput.value);
+    itemPrices.push(Number(itemPriceInput.value));
+
+    itemDisplay.textContent = itemDisplay.textContent + itemNames[itemNames.length - 1] + " for $" + itemPrices[itemPrices.length - 1] + "\n";
+    
+    itemNameInput.value = ""
+    itemPriceInput.value = ""
+    calculate();
+};
+
+
+removeItemButton.onclick = function()
+{
+    itemDisplay.textContent = itemDisplay.textContent.replace(itemNames[itemNames.length - 1] + " for $" + itemPrices[itemPrices.length - 1] + "\n", "");
+    
+    itemNames.pop();
+    itemPrices.pop();
+
+    calculate();
+}
+
+function calculate()
+{
+    let total = 0
+    for (i in itemPrices)
+        {
+            itemTax = itemPrices[i] * tax;
+            total += itemPrices[i] + itemTax;
+        }
+    
+    total = Math.round(total*100)/100;
+    totalDisplay.textContent = "Total: $" + total;
+}
